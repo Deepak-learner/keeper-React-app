3ae6dca2e4beaee862c9ae75fccc5852036ddcf4
unknown <deepak710agarwal@gmail.com>
2020-03-12 00:35:17 +0530

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  
+  return (
+    <footer>
+      <h3>Created with React 👨‍💻</h3>
+      
+      <h3>Created by DA 😎. </h3>
+      </footer>
+  );
+}
+
+export default Footer;
